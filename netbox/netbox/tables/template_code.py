47ebc983f5a656b2b98ffed117c7aa8ef331790b
Jeremy Stretch <jstretch@netboxlabs.com>
2023-10-31 16:34:49 -0400

    Linkify object attributes

--- netbox/netbox/tables/template_code.py
+++ netbox/netbox/tables/template_code.py
@@ -1,5 +1,11 @@
 SEARCH_RESULT_ATTRS = """
 {% for name, value in record.display_attrs.items %}
-  <span class="badge bg-secondary">{{ name|bettertitle }}: {{ value }}</span>
+  {% with url=value.get_absolute_url %}
+    <span class="badge bg-secondary">
+      {% if url %}<a href="{{ url }}">{% endif %}
+      {{ name|bettertitle }}: {{ value }}
+      {% if url %}</a>{% endif %}
+    </span>
+  {% endwith %}
 {% endfor %}
 """
