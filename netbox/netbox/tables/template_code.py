f181b14858a94bbe0aad522392a1f64c5195fc4b
Jeremy Stretch <jstretch@netboxlabs.com>
2023-11-01 14:58:28 -0400

    Use tooltips for display attributes

--- netbox/netbox/tables/template_code.py
+++ netbox/netbox/tables/template_code.py
@@ -1,9 +1,9 @@
 SEARCH_RESULT_ATTRS = """
 {% for name, value in record.display_attrs.items %}
   {% with url=value.get_absolute_url %}
-    <span class="badge bg-secondary">
+    <span class="badge bg-secondary" data-bs-toggle="tooltip" data-bs-placement="bottom" title="{{ name|bettertitle }}">
       {% if url %}<a href="{{ url }}">{% endif %}
-      {{ name|bettertitle }}: {{ value }}
+      {{ value }}
       {% if url %}</a>{% endif %}
     </span>
   {% endwith %}
