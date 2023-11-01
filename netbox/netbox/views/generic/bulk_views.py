860e880e0e677756ede7549b026c7e5cfbc72212
Jeremy Stretch <jstretch@netboxlabs.com>
2023-11-01 15:59:36 -0400

    Condense logic

--- netbox/netbox/views/generic/bulk_views.py
+++ netbox/netbox/views/generic/bulk_views.py
@@ -689,16 +689,14 @@ class BulkRenameView(GetReturnURLMixin, BaseMultiObjectView):
 
             find = form.cleaned_data['find']
             replace = form.cleaned_data['replace']
-            if obj.name:
-                if form.cleaned_data['use_regex']:
-                    try:
-                        obj.new_name = re.sub(find, replace, obj.name or '')
-                    # Catch regex group reference errors
-                    except re.error:
-                        obj.new_name = obj.name
-                else:
-                    if obj.name:
-                        obj.new_name = obj.name.replace(find, replace)
+            if obj.name and form.cleaned_data['use_regex']:
+                try:
+                    obj.new_name = re.sub(find, replace, obj.name)
+                # Catch regex group reference errors
+                except re.error:
+                    obj.new_name = obj.name
+            elif obj.name:
+                obj.new_name = obj.name.replace(find, replace)
             else:
                 obj.new_name = obj.name
 
