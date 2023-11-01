bfbcc6bbac6985937089c013f6dcd95a2d2c52d9
Jeremy Stretch <jstretch@netboxlabs.com>
2023-11-01 15:03:33 -0400

    Simplify template code

--- netbox/netbox/tables/template_code.py
+++ netbox/netbox/tables/template_code.py
@@ -1,11 +1,7 @@
 SEARCH_RESULT_ATTRS = """
 {% for name, value in record.display_attrs.items %}
-  {% with url=value.get_absolute_url %}
-    <span class="badge bg-secondary" data-bs-toggle="tooltip" data-bs-placement="bottom" title="{{ name|bettertitle }}">
-      {% if url %}<a href="{{ url }}">{% endif %}
-      {{ value }}
-      {% if url %}</a>{% endif %}
-    </span>
-  {% endwith %}
+  <span class="badge bg-secondary" data-bs-toggle="tooltip" data-bs-placement="bottom" title="{{ name|bettertitle }}">
+    {{ value|linkify }}
+  </span>
 {% endfor %}
 """
